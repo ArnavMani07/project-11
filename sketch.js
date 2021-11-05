a5cc13471a9dc451a959b018be362048828c8490
ArnavMani07 <88050563+ArnavMani07@users.noreply.github.com>
2021-11-05 13:07:00 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,25 @@
+var weight = [35,38,42,45,43,34,36,41,48,32];
+
+var sum = 0;
+
+
+  function weight_average () {
+    for(i=0;i<weight.length;i++) {
+var sum = 0;
+sum = sum + weight[i]
+    }
+    console.log (sum);
+  }
+ 
+function setup() {
+  createCanvas(400,400);
+weight_average ();
+}
+
+function draw() 
+{
+  background(30);
+}
+
+ 
+
